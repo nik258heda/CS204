b27d972945e8fc2491d5eb57dc28d5515b4df0c5
nik258heda <45728865+nik258heda@users.noreply.github.com>
2019-08-05 16:49:16 +0530

    Add files via upload

---
+++ lab2_linkedlist.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+	int x;
+	int y;
+	struct Node *next;	
+};
+struct Node* head =NULL;
+float dis(int x, int y){
+	float k;
+	k=sqrt(x*x + y*y);
+	return k;
+}
+void add_first(int x1,int y1){
+	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+	new_node->x=x1;
+	new_node->y=y1;
+	new_node->next=head;
+	head =new_node;	
+}
+void del_first(){
+	Node *temp=head;
+	temp=temp->next;
+	head=temp->next;
+	delete temp;
+}
+void del(int x1,int y1) 
+{ 
+    struct Node* temp =head, *prev; 
+    if (temp != NULL && temp->x == x1 && temp->y == y1) 
+    { 
+        head = temp->next;    
+        free(temp);                
+        return; 
+    }  
+    while (temp != NULL && (temp->x != x1 || temp->y != y1)) 
+    { 
+        prev = temp; 
+        temp = temp->next; 
+    }  
+    if (temp == NULL) return; 
+    prev->next = temp->next; 
+    free(temp);
+} 
+bool search(int x1,int y1) 
+{ 
+    struct Node* current = head; 
+    while (current != NULL) 
+    { 
+        if (current->x == x1 && current->y == y1) 
+            return true; 
+        current = current->next; 
+    } 
+    return false; 
+} 
+void search_dis(float d) 
+{ 
+    struct Node* current = head; 
+    while (current != NULL) 
+    { 
+	int a= current->x;
+	int b= current->y;
+        if (dis(a,b)<=d) 
+            cout<<"("<<current->x<<","<<current->y<<")"; 
+        current = current->next; 
+    } 
+    return;  
+}
+int length()  
+{  
+    int count = 0; 
+    Node* current = head;  
+    while (current != NULL)  
+    {  
+        count++;  
+        current = current->next;  
+    }  
+    return count;  
+}  
+int main(){
+	long long t;
+	cin>>t;
+	int i;
+	for(long long j=0;j<t;j++){
+		cin>>i;
+		if(i==1){
+			int x1,y1;
+			cin>>x1>>y1;
+			add_first(x1,y1);
+		}
+		if(i==2){
+			del_first();
+		}
+		if(i==3){
+			int x1,y1;
+			cin>>x1>>y1;
+			del(x1,y1);
+		}
+		if(i==4){
+			int d;
+			cin>>d;
+			search_dis(d);
+			cout<<"\n";
+		}
+		if(i==5){
+			int x1,y1;
+			cin>>x1>>y1;
+			cout<<search(x1,y1)<<"\n";
+		}
+		if(i==6){
+			cout<<length()<<"\n";
+		}	
+	}
+	return 0;
+}
+
