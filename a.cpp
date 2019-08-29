452c120ba63aa15db733e0151d0d10ac82fae2c4
nik258heda <45728865+nik258heda@users.noreply.github.com>
2019-08-29 09:09:32 +0530

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,331 +1,237 @@
 #include <bits/stdc++.h>
-#define lli long long int
-#define pb push_back
-#define vt vector
-#define f(x, y, z) for (lli x = y; x < z; x++)
 using namespace std;
-bool vis[26];
-lli ar[26] = {
-    0,
-};
-
-string itos(string ins)
-{
-    bool check = false;
-    string temp = "";
-    if (ar[ins[0] - 'a'] < 0)
-    {
-        check = true;
-    }
-    lli dr = abs(ar[ins[0] - 'a']);
-    while (dr > 0)
-    {
-        int tmp = dr % 10;
-        temp.pb(tmp + '0');
-        dr /= 10;
-    }
-    if (check)
-    {
-        temp = '-' + temp;
-    }
-    return temp;
-}
-
-int isoperator(string c)
-{
-    if (c == "^" || c == "/" || c == "*" || c == "+" || c == "-" || c == "(")
-        return 1;
-    return 0;
-}
-struct node
-{
-    string s;
-    node *left = NULL, *right = NULL;
-};
-node *construct_tree(vt<string> postfix)
-{
-    node *root = NULL;
-    vt<node *> stk;
-    int i = 0;
-    while (i != postfix.size())
-    {
-        if (postfix[i] != "+" && postfix[i] != "-" && postfix[i] != "*" && postfix[i] != "/" && postfix[i] != "^")
-        {
-            node *temp = (node *)malloc(sizeof(node));
-            temp->s = postfix[i];
-            f(j, 0, postfix[i].size()) if (postfix[i][j] < '0' || postfix[i][j] > '9') return NULL;
-            stk.pb(temp);
-        }
-        else
-        {
-            if (stk.size() < 2)
-                return NULL;
-            node *s1 = stk.back();
-            stk.pop_back();
-            node *s2 = stk.back();
-            stk.pop_back();
-            node *temp = (node *)malloc(sizeof(node));
-            temp->s = postfix[i];
-            temp->left = s2;
-            temp->right = s1;
-            stk.pb(temp);
-        }
-        i++;
-    }
-    if (stk.size() != 1)
-        return NULL;
-    return stk.back();
-}
-vector<string> stov(string s)
-{
-    int n = s.size();
-    vector<string> st;
-    for (int i = 0; i < n; i++)
-    {
-        string temp = "";
-        if ((s[i] >= '0' && s[i] <= '9'))
-        {
-            int j = i;
-            for (; (s[j] >= '0' && s[j] <= '9'); j++)
-            {
-                temp += s[j];
-            }
-            i = j - 1;
-        }
-        else
-        {
-            temp += s[i];
-        }
-        st.push_back(temp);
-    }
-    return st;
-}
-int inprec(char input)
-{
-
-    switch (input)
-    {
-    case '+':
-    case '-':
-        return 2;
-    case '*':
-    case '%':
-    case '/':
-        return 4;
-    case '^':
-        return 5;
-    case '(':
-        return 0;
-    }
-}
-
-int outprec(char input)
-{
-
-    switch (input)
-    {
-    case '+':
-    case '-':
-        return 1;
-    case '*':
-    case '%':
-    case '/':
-        return 3;
-    case '^':
-        return 6;
-    case '(':
-        return 100;
-    }
-}
-
-vector<string> itop(vector<string> s)
-{
-    stack<string> st;
-    st.push("N");
-    int l = s.size();
-
-    vector<string> ns;
-
-    for (int i = 0; i < l; i++)
-
-    {
-
-        if ((s[i][0] >= '0' && s[i][0] <= '9'))
-
-            ns.push_back(s[i]);
-
-        else if (s[i] == "(")
-
-            st.push("(");
-
-        else if (s[i] == ")")
-
-        {
-
-            while (st.top() != "N" && st.top() != "(")
-
-            {
-
-                string c = st.top();
-
-                st.pop();
-
-                ns.push_back(c);
-            }
-
-            if (st.top() == "(")
-
-            {
-
-                string c = st.top();
-
-                st.pop();
-            }
-        }
-
-        else
-        {
-
-            while (st.top() != "N" && outprec(s[i][0]) < inprec((st.top())[0]))
-
-            {
-
-                string c = st.top();
-
-                st.pop();
-
-                ns.push_back(c);
-            }
-
-            st.push(s[i]);
-        }
-    }
-
-    while (st.top() != "N")
-
-    {
-
-        string c = st.top();
-
-        st.pop();
-        ns.push_back(c);
-    }
-    return ns;
-}
-
-lli evaluate(node *root)
-{
-    string s = root->s;
-
-    lli ans = 0;
-
-    if (s != "+" && s != "-" && s != "*" && s != "/" && s != "^")
-    {
-        return stoi(s);
-    }
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define si size()
+#define bk back()
+#define popb pop_back()
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl "\n"
+#define lb lower_bound
+#define ub upper_bound
+#define emp empty()
+#define beg begin()
+#define en end()
+#define forl(i,a,n) for(int i=a;i<n;i++)
+#define rfor(i,n,a) for(int i=n;i>=a;i--)
+#define fore(i,a,n) for(int i=a;i<=n;i++)
+ll mod=1e9+7;
+//------------------------------------------------------------
+int prec(char c) 
+{ 
+    if(c == '^') 
+    return 3; 
+    else if(c=='$')
+	return 4;
+    else if(c == '*' || c == '/') 
+    return 2; 
+    else if(c == '+' || c == '-') 
+    return 1; 
     else
-    {
-        int l = evaluate(root->left), r = evaluate(root->right);
-        if (s == "+")
-        {
-            ans = l + r;
-        }
-        else if (s == "-")
-        {
-            ans = l - r;
-        }
-        else if (s == "*")
-        {
-            ans = l * r;
-        }
-        else if (s == "/")
-        {
-            ans = l / r;
-        }
-        else
-        {
-            ans = 1;
-            f(i, 0, r)
-            {
-                ans *= l;
-            }
-        }
-    }
-    return ans;
+    return -1; 
+} 
+struct node{
+	int x;
+	char opr;
+	bool b;
+};
+struct et{
+	node val;
+	et * left;
+	et * right;
+};
+
+et * make_tree(vector<node>v){
+	stack<et *> s;
+	et *t, *t1, *t2;
+	forl(i,0,v.si){
+		if(v[i].b){
+			t=new et;
+			t->val=v[i];
+			t1=s.top();
+			s.pop();
+			t2=s.top();
+			s.pop();
+			t->right=t1;
+			t->left=t2;
+			s.push(t);
+		}
+		else{
+			t=new et;
+			t->val=v[i];
+			t->left=NULL;
+			t->right=NULL;
+			s.push(t);
+		}
+	}
+	return s.top();
 }
-int main()
-{
-    lli t, n;
-    cin >> t;
-    while (t--)
-    {   
-        memset(vis, false, sizeof(vis));
-        memset(ar, 0, sizeof(ar));
-        cin >> n;
-        while (n--)
-        {
-            string s;
-            cin >> s;
-            bool c= false;
-            if(s.size() > 1 && s[1] == '='){
-                c =true;
-            }
-            string st="";
-            bool d = false;
-            if(c){
-                vis[s[0]]= true;
-                for(lli i = 2; i<s.size(); i++){
-                    if(s[i] >= 'a' && s[i] <= 'z'){
-                        if(!vis[s[i]]){
-                            d = true;
-                            break;
-                        }
-                        else{string m = "";
-                                m.pb(s[i]);
-                            string sz = itos(m);
-                            for(lli q=0; q<sz.size(); q++){
-                                st.pb(sz[q]);
-                            }
-                        }
-                    }
-                    else{
-                        st.pb(s[i]);
-                    }
-                }
-            }else{
-                for(lli i = 0; i<s.size(); i++){
-                    if(s[i] >= 'a' && s[i] <= 'z'){
-                        if(!vis[s[i]]){
-                            d = true;
-                            break;
-                        }
-                        else{string m = "";
-                                m.pb(s[i]);
-                            string sz = itos(m);
-                            for(lli q=0; q<sz.size(); q++){
-                                st.pb(sz[q]);
-                            }
-                        }
-                    }
-                    else{
-                        st.pb(s[i]);
-                    }
-                }
-            }
-            if(d){cout<<"CANT BE EVALUATED\n";
-            continue;}
-            vector<string> str = stov(st);
-            str = itop(str);
-            node *root = construct_tree(str);
-            if (root == NULL)
-                cout << "CANT BE EVALUATED" << endl;
-            else
-                if(c){
-                    ar[s[0]-'a'] = evaluate(root);
-                }
-                else{
-                    cout<<evaluate(root)<<endl;
-                }
-        }
-    }
+stack<node>st;
+vector<node> intopost(vector<node>v){
+	vector<node> ans;
+	forl(i,0,v.si){
+		if(v[i].b){
+			char c=v[i].opr;
+			if(c=='('){
+				st.push(v[i]);
+			}
+			else if(c==')'){
+				while(st.top().opr!='('){
+					ans.pb(st.top());
+					st.pop();
+				}
+				st.pop();
+			}
+			else{
+				while(st.top().opr!='('&&prec(c)<=prec(st.top().opr)){
+					if(prec(c)==3&&prec(st.top().opr)==3) break;
+					if(prec(c)==4&&prec(st.top().opr)==4) break;
+					ans.pb(st.top());
+					st.pop();
+				}
+				//if(st.top().opr=='(') st.pop();
+				st.push(v[i]);
+			}
+		}
+		else{
+			ans.pb(v[i]);
+		}
+	}
+	return ans;
+	
+}
+int eval(et * root){
+	if(root==NULL) return 0;
+	if(root->left==NULL&&root->right==NULL){
+		return root->val.x;
+	}
+	int l=eval(root->left);
+	int r=eval(root->right);
+	if(root->val.opr=='+') return l+r;
+	else if(root->val.opr=='-'||root->val.opr=='$') return l-r;
+	else if(root->val.opr=='*') return l*r;
+	else if(root->val.opr=='/') return l/r;
+	else return pow(l,r);
+}
+int main(){
+	fastio;
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		//map<string,int> m;
+		vector<string> key;
+		vector<int> val;
+		while(n--){
+			string var="",curr_var="";
+			string s="(";
+			string tmps;
+			cin>>tmps;
+			int c=1; int check=1;
+			forl(i,0,tmps.si){
+				if(!(tmps[i]>='a'&&tmps[i]<='z'||tmps[i]>='A'&&tmps[i]<='Z')&&(tmps[i]!='=')){
+					if(curr_var.si!=0){
+						//if(m.find(curr_var)==m.end()){ cout<<"CANT BE EVALUATED"<<endl; check=0;}
+						int x=-1;
+						forl(j,0,key.si){
+							if(key[j]==curr_var){
+								x=j; break;
+							}
+						}
+						if(x==-1) {cout<<"CANT BE EVALUATED"<<endl; check=0;}
+						else {s+=to_string(val[x]); curr_var="";}
+					}
+				}
+				if(tmps[i]>='0'&&tmps[i]<='9'){
+					c=0;
+					s+=tmps[i];
+				}
+				else if(tmps[i]=='('||tmps[i]==')'){
+					s+=tmps[i];
+				}
+				else if(tmps[i]>='a'&&tmps[i]<='z'||tmps[i]>='A'&&tmps[i]<='Z'){
+					curr_var+=tmps[i];
+				}
+				else if(tmps[i]=='='){
+					var=curr_var;
+					curr_var="";
+					continue;
+				}
+				else{
+					if(c!=0&&tmps[i]=='-'){
+						s+="0$";
+					}
+					else s+=tmps[i];
+					c++;
+				}
+				if(check==0) break;
+			}
+			
+			if(curr_var.si!=0){
+					
+				//if(m.find(curr_var)==m.end()){ cout<<"CANT BE EVALUATED"<<endl; check=0;}
+				int x=-1;
+				forl(j,0,key.si){
+					if(key[j]==curr_var){
+						x=j; break;
+					}
+				}
+				if(x==-1){ cout<<"CANT BE EVALUATED"<<endl; check=0;}
+				else {s+=to_string(val[x]); curr_var="";}
+				    
+			}
+			if(check==0) continue;
+			s+=')';
+			int a=0,o=0;
+			string curr="";
+			vector<node> v;
+			forl(i,0,s.si){
+				if(s[i]>='0'&&s[i]<='9'){
+					curr+=s[i];
+				}
+				else{
+					if(!curr.si&&s[i]=='-'){
+						curr+=s[i];
+					}
+					else if(curr.si==1&&curr[0]=='-'){
+						curr+='1';
+						s[i-1]='*';
+						i-=2;
+					}
+					else {
+						if(curr.si){
+						node n;
+						n.b=0;
+						n.x=stoi(curr);
+						v.pb(n);}
+						curr="";
+						node n;
+						n.opr=s[i];
+						n.b=1;
+						v.pb(n);
+					}
+				}
+			}
+			if(curr.si){
+				node n;
+				n.b=0;
+				n.x=stoi(curr);
+				v.pb(n);
+			}
+			vector<node> post=intopost(v);
+			et * root=make_tree(post);
+			if(var.si==0)cout<<eval(root)<<endl;
+			else{
+				//m[var]=eval(root);
+				key.pb(var);
+				val.pb(eval(root));
+			} 
+		}
+	}
+	return 0;
 }
