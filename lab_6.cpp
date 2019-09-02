8abf8652c224331021ae004103f2baefea5d2b86
nik258heda <45728865+nik258heda@users.noreply.github.com>
2019-09-02 16:09:38 +0530

    Update lab_6.cpp

--- lab_6.cpp
+++ lab_6.cpp
@@ -1,30 +1,56 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long int 
-int main(){
-    pair<ll,ll> m;
-    m={-1,-1};
+#define ll long long int
+int main()
+{
+    pair<ll, ll> m;
+    m = {-1, -1};
     ll q;
     ll n;
-    cin>>n>>q;
-    vector<ll> a(n+1,0);
-    int x=0;
-    while(q--){
-        cin>>x;
-        if(x==1){
-            int b,c;
-            cin>>b>>c;
-            a[b]=a[b]+c;
-            if(a[b]>m.second){
-                m={b,a[b]};
+    cin >> n >> q;
+    vector<pair<ll, ll>> a;
+    int x = 0;
+    while (q--)
+    {
+        cin >> x;
+        if (x == 1)
+        {
+            ll b, c, i;
+            cin >> b >> c;
+            if (a.empty())
+            {
+                a.push_back({b, c});
+            }
+            else
+            {
+                for (i = 0; i < a.size(); i++)
+                {
+                    if (b == a[i].first)
+                    {
+                        a[i].second += c;
+                        c=a[i].second;
+                        break;
+                    }
+                }
+                if (i == a.size())
+                {
+                    a.push_back({b,c});
+                }
+            }
+            if (c > m.second)
+            {
+                m = {b,c};
             }
         }
-        if(x==2){
-            if(m.first==-1){
-                cout<<"Data Not Found";
+        if (x == 2)
+        {
+            if (m.first == -1)
+            {
+                cout << "Data Not Found";
             }
-            else{
-                cout<<m.first<<"\n";
+            else
+            {
+                cout << m.first << "\n";
             }
         }
     }
